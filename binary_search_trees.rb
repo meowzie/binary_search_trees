65bcf0545635377d6e1788114931dc75838790f0
meowzie <appenge2@gmail.com>
2022-01-27 16:19:46 +0200

    Created @list variable, which stores the initial array used for the tree; Added functionality to build trees and to print them in a tree structure

--- binary_search_trees.rb
+++ binary_search_trees.rb
@@ -2,19 +2,60 @@
 
 # creates new items in the binary search tree
 class Node
+  attr_accessor :data, :right, :left
+
   def initialize(value)
-    @value = value
+    @data = value
+    @right = nil
+    @left = nil
   end
 end
 
 # creates the tree
 class Tree
-  def initialize
-    @root = nil
+  attr_accessor :root, :list
+
+  def initialize(array)
+    @root = 0
+    @list = array.sort.uniq.map { |value| Node.new(value) }
   end
 
-  def build_tree(array)
-    # not yet done
-    array.sort!.uniq!
+  def pretty(node = @root, prefix = '', is_left = true)
+    pretty(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
+
+  def halver(array, left_truthiness)
+    case left_truthiness
+    when true
+      array[0...array.length / 2]
+    when false
+      array[array.length / 2 + 1...array.length]
+    end
+  end
+
+  def root_setter(left, right)
+    @root.left = left[left.length / 2] unless left[left.length / 2] == @root
+    @root.right = right[right.length / 2] unless right[right.length / 2] == @root
+  end
+
+  def build_tree(array = @list)
+    return if array.length < 2
+
+    left = halver(array, true)
+    right = halver(array, false)
+    @root = array[array.length / 2]
+    root_setter(left, right)
+    @root = @list[@list.length / 2]
+    build_tree(left)
+    build_tree(right)
+    @root.data
   end
 end
+
+array = []
+100.times { array << rand(100) }
+tree = Tree.new(array)
+tree.build_tree
+tree.pretty
